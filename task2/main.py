a780d0b46ff0f2094a881373bab3e18e36fbda65
Danis Sharipov <danshadance@mail.ru>
2023-02-14 12:19:35 +0600

    homework6 hotfix done

--- task2/main.py
+++ task2/main.py
@@ -2,19 +2,25 @@
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
 
 import os
-from modules.func import Find_Index
+from modules.func import *
 
 os.system('cs||clear')
 
-my_list = [1,2,3,4,5,6,7,8,9,10]
+my_list = FillRnd()
 
+print("Сенерированный массив: ", my_list)
+
+print()
 min = int(input("Введите, пожалуйста, значение минимальной границы: "))
 print()
 max = int(input("Введите, пожалуйста, значение максимальной границы: "))
 print()
-
+enter_func = input("Нажмите Enter, чтобы запустить функцию ... ")
 range_list = Find_Index(my_list, min, max)
 
 print()
 print("Индексы значений: ", range_list)
 
+print()
+exit_programm = input("Нажмите Enter, чтобы завершить программу ... ")
+os.system('cs||clear')
--- task2/modules/func.py
+++ task2/modules/func.py
@@ -1,3 +1,4 @@
+import random
 
 def Find_Index(my_list, min, max):
     range_list = []
@@ -5,6 +6,14 @@ def Find_Index(my_list, min, max):
         if my_list[i] > min and my_list[i] < max:
             range_list.append(i)
             print()
-            print("Индекс элемента в заданном диапозоне: ", '[', i, ']',my_list[i])
+            print("Индекс элемента в заданном диапозоне: ", '[', i, ']', my_list[i])
     range_list.sort()
     return range_list
+
+def FillRnd():
+    my_list = []
+    for i in range(1,10):
+        my_list.append(random.randint(1,10))
+    my_list.sort()
+    print()
+    return my_list
